f2362b018fa292b5af97971c9378ed5aaf30fceb
Hironsan <hiroki.nakayama.py@gmail.com>
2018-05-14 18:28:23 +0900

    Add models

--- doccano/app/server/models.py
+++ doccano/app/server/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    text = models.CharField(max_length=100)
+
+
+class Annotation(models.Model):
+    text = models.TextField()
+    prob = models.FloatField()
+    labels = models.ManyToManyField(Label)
+    # users = models.ManyToManyField(User)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=32)
+    # password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    # users = models.ManyToManyField(User)
