6bc93d900ac76aaf4b8c60674e45ce275616a667
Hironsan <hiroki.nakayama.py@gmail.com>
2018-06-08 09:48:54 +0900

    Add link for project list page to annotation page

--- doccano/app/server/urls.py
+++ doccano/app/server/urls.py
@@ -6,7 +6,7 @@ from .views import ProjectListView, ProjectAdminView, RawDataAPI, LabelAPI
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
     path('<int:pk>/admin', ProjectAdminView.as_view()),
-    path('<int:project_id>/', AnnotationView.as_view()),
+    path('<int:project_id>/', AnnotationView.as_view(), name='annotation'),
     path('<int:project_id>/apis/data', AnnotationAPIView.as_view()),
     path('<int:pk>/apis/raw_data', RawDataAPI.as_view(), name='data_api'),
     path('<int:pk>/apis/labels', LabelAPI.as_view(), name='label_api'),
