f2b0ea8a803dcf724d4e69e6f2abd6ba5a73af62
Hironsan <hiroki.nakayama.py@gmail.com>
2018-06-08 11:44:53 +0900

    Update project list template

--- doccano/app/server/urls.py
+++ doccano/app/server/urls.py
@@ -5,7 +5,7 @@ from .views import ProjectListView, ProjectAdminView, RawDataAPI, LabelAPI
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
-    path('<int:pk>/admin', ProjectAdminView.as_view()),
+    path('<int:pk>/admin', ProjectAdminView.as_view(), name='project-admin'),
     path('<int:project_id>/', AnnotationView.as_view(), name='annotation'),
     path('<int:project_id>/apis/data', AnnotationAPIView.as_view()),
     path('<int:pk>/apis/raw_data', RawDataAPI.as_view(), name='data_api'),
