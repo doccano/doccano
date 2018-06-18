af04c863dcbddc2f19d7c59b7593cfaa05f4ec38
Hironsan <light.tree.1.13@gmail.com>
2018-06-18 10:02:24 +0900

    Change url

--- doccano/app/server/urls.py
+++ doccano/app/server/urls.py
@@ -6,9 +6,9 @@ from .views import ProjectListView, ProjectAdminView, RawDataAPI, LabelAPI, Data
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
     path('<int:pk>/admin', ProjectAdminView.as_view(), name='project-admin'),
-    path('<int:project_id>/', AnnotationView.as_view(), name='annotation'),
+    #path('<int:project_id>/', AnnotationView.as_view(), name='annotation'),
     path('<int:project_id>/download', DataDownloadAPI.as_view(), name='download'),
-    path('<int:project_id>/inbox', InboxView.as_view(), name='inbox'),
+    path('<int:project_id>/', InboxView.as_view(), name='annotation'),
     path('<int:project_id>/apis/data', AnnotationAPIView.as_view()),
     path('<int:pk>/apis/raw_data', RawDataAPI.as_view(), name='data_api'),
     path('<int:pk>/apis/labels', LabelAPI.as_view(), name='label_api'),
