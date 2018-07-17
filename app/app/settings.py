0bbe25455d6dc9532c89a814c58d2bc4821973cf
Hironsan <light.tree.1.13@gmail.com>
2018-07-17 16:11:42 +0900

    Add django-heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/projects/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
