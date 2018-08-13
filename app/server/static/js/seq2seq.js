d76441e80ad0af2ef28a3adaa3fa7e916e722511
Hironsan <light.tree.1.13@gmail.com>
2018-08-13 14:19:57 +0900

    Refactor seq2seq.js

--- app/server/static/js/seq2seq.js
+++ app/server/static/js/seq2seq.js
@@ -1,73 +1,75 @@
 import Vue from 'vue';
+import annotationMixin from './mixin';
+import HTTP from './http';
+
 Vue.use(require('vue-shortkey'));
-import annotationMixin from './mixin.js';
-import HTTP from './http.js';
 
 
-var vm = new Vue({
-    el: '#mail-app',
-    delimiters: ['[[', ']]'],
-    data: {
-        newTodo: '',
-        editedTodo: null
+const vm = new Vue({
+  el: '#mail-app',
+  delimiters: ['[[', ']]'],
+  data: {
+    newTodo: '',
+    editedTodo: null,
+  },
+  mixins: [annotationMixin],
+  directives: {
+    'todo-focus': function(el, binding) {
+      if (binding.value) {
+        el.focus();
+      }
     },
-    mixins: [annotationMixin],
-    directives: {
-        'todo-focus': function (el, binding) {
-            if (binding.value) {
-                el.focus()
-            }
-        }
+  },
+
+  methods: {
+    addTodo() {
+      const value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+
+      const docId = this.docs[this.pageNumber].id;
+      const payload = {
+        text: value,
+      };
+      HTTP.post(`docs/${docId}/annotations/`, payload).then((response) => {
+        this.annotations[this.pageNumber].push(response.data);
+      });
+
+      this.newTodo = '';
     },
-    methods: {
-        addTodo: function () {
-            var value = this.newTodo && this.newTodo.trim()
-            if (!value) {
-                return
-            }
 
-            var doc_id = this.items[this.cur].id;
-            var payload = {text: value}
-            HTTP.post(`docs/${doc_id}/annotations/`, payload).then(response => {
-                this.items[this.cur]['labels'].push(response.data)
-            })
-
-            this.newTodo = ''
-        },
-
-        removeTodo: function (todo) {
-            var doc_id = this.items[this.cur].id;
-            HTTP.delete(`docs/${doc_id}/annotations/${todo.id}`).then(response => {
-                this.items[this.cur]['labels'].splice(this.items[this.cur]['labels'].indexOf(todo), 1)
-            });
-        },
-
-        editTodo: function (todo) {
-            this.beforeEditCache = todo.text
-            this.editedTodo = todo
-        },
-
-        doneEdit: function (todo) {
-            if (!this.editedTodo) {
-                return
-            }
-            this.editedTodo = null
-            todo.text = todo.text.trim()
-            if (!todo.text) {
-                this.removeTodo(todo)
-            }
-            var doc_id = this.items[this.cur].id;
-            HTTP.put(`docs/${doc_id}/annotations/${todo.id}`, todo).then(response => {
-                console.log(response)
-            });
-        },
-
-        cancelEdit: function (todo) {
-            this.editedTodo = null
-            todo.text = this.beforeEditCache
-        }
+    removeTodo(todo) {
+      const docId = this.docs[this.pageNumber].id;
+      HTTP.delete(`docs/${docId}/annotations/${todo.id}`).then((response) => {
+        const index = this.annotations[this.pageNumber].indexOf(todo);
+        this.annotations[this.pageNumber].splice(index, 1);
+      });
     },
-    created: function () {
-        this.submit();
-    }
+
+    editTodo(todo) {
+      this.beforeEditCache = todo.text;
+      this.editedTodo = todo;
+    },
+
+    doneEdit(todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.text = todo.text.trim();
+      if (!todo.text) {
+        this.removeTodo(todo);
+      }
+      const docId = this.docs[this.pageNumber].id;
+      HTTP.put(`docs/${docId}/annotations/${todo.id}`, todo).then((response) => {
+        console.log(response);
+      });
+    },
+
+    cancelEdit(todo) {
+      this.editedTodo = null;
+      todo.text = this.beforeEditCache;
+    },
+  },
 });
