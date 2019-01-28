6fca7d1337c83ee19bb08fd9cda8e635ee2927c1
Clemens Wolff <clewolff@microsoft.com>
2019-01-28 10:47:15 -0500

    Make usage of whitenoise runserver explicit

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ if os.environ.get('DEBUG') == 'False':
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
