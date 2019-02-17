27fd1abe1676c3c178b1995ded2865a83310bb86
Omri Allouche <omri.allouche@gmail.com>
2019-02-17 12:13:06 +0200

    Text classifier changed to Logistic Regression. Print train and test classification report.

--- app/classifier/text/text_classifier.py
+++ app/classifier/text/text_classifier.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 import re
 
@@ -19,7 +21,7 @@ def process_text(x):
 
 def run_model_on_file(input_filename, output_filename, user_id, method='bow'):
     # nlp = spacy.load("en_core_web_sm")
-
+    print('Reading input file...')
     df = pd.read_csv(input_filename)
 
     # df_labeled = df_labeled[['text', 'label_id']]
@@ -65,15 +67,29 @@ def run_model_on_file(input_filename, output_filename, user_id, method='bow'):
         return tmp_df
 
     X, y = df_to_matrix(df[ ~pd.isnull(df['label']) ])
+    y = y.values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-    model_params = {'bootstrap': True, 'criterion': 'gini', 'max_depth': 16, 'max_features': 'sqrt', 'min_samples_leaf': 16, 'min_samples_split': 16, 'n_estimators': 200}
-    model = RandomForestClassifier(**model_params)
+    print('Training the model...')
+
+    # model_params = {'bootstrap': True, 'criterion': 'gini', 'max_depth': 16, 'max_features': 'sqrt', 'min_samples_leaf': 16, 'min_samples_split': 16, 'n_estimators': 200}
+    # model = RandomForestClassifier(**model_params)
+
+    model = LogisticRegression(verbose=True)
     model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_train)
+    print('Performance on train set:')
+    print(metrics.classification_report(y_train, y_pred))
+
+    print('Performance on test set:')
+    y_pred = model.predict(X_test)
+    print(metrics.classification_report(y_test, y_pred))
 
     columns=['text', 'label', 'prediction', 'is_error', 'confidence']
     tmp_df = df.copy()
     tmp_df['label'] = None
+    print('Running the model on the entire dataset...')
     tmp_df = run_model(df)
 
     tmp_df['user_id'] = user_id
@@ -81,6 +97,12 @@ def run_model_on_file(input_filename, output_filename, user_id, method='bow'):
                             'id': 'document_id'}, axis=1)
     tmp_df['label_id'] = tmp_df['prediction']
     # save to CSV file
+    print('Saving output...')
     tmp_df[['document_id', 'label_id', 'user_id', 'prob']].to_csv(output_filename, index=False, header=True)
 
+    print('Done running the model!')
     return tmp_df
+
+
+if __name__ == '__main__':
+    run_model_on_file('../../ml_input.csv', '../../ml_out_manual.csv', user_id=2)
