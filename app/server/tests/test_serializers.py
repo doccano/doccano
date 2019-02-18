accbaa83b18a5e4e9ce90c6caa294461afb26d1b
Hironsan <light.tree.1.13@gmail.com>
2019-02-18 15:39:53 +0900

    WIP

---
+++ app/server/tests/test_serializers.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from mixer.backend.django import mixer
+from ..serializers import LabelSerializer
+
+from ..models import Label, DocumentAnnotation, SequenceAnnotation, Seq2seqAnnotation
+from ..serializers import ClassificationDocumentSerializer, DocumentAnnotationSerializer
+from ..serializers import SequenceDocumentSerializer, SequenceAnnotationSerializer
+from ..serializers import Seq2seqDocumentSerializer, Seq2seqAnnotationSerializer
+
+
+class TestLabelSerializer(TestCase):
+
+    def test_create(self):
+        label = mixer.blend('server.Label')
+        serializer = LabelSerializer(label)
+        self.assertIsInstance(serializer.data, dict)
+
+    def test_annotation(self):
+        from rest_framework import serializers
+        from ..models import Seq2seqDocument
+        class HogeSerializer(serializers.ModelSerializer):
+            seq2seq_annotations = Seq2seqAnnotationSerializer(many=True)
+
+            class Meta:
+                model = Seq2seqDocument
+                fields = ('id', 'text', 'seq2seq_annotations')
+
+        doc = mixer.blend('server.Seq2seqDocument')
+        ann = mixer.blend('server.Seq2seqAnnotation')
+        doc.seq2seq_annotations.add(ann)
+        serializer = HogeSerializer(doc)
+        print(serializer.data)
