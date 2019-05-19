bfb9f0ccd701a7073e7b8af5b59d144f5f6eaee0
Vit Rybn <strelok2010@gmail.com>
2019-05-19 14:06:46 +0800

    Added vue-good-table

--- app/server/webpack.config.js
+++ app/server/webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
