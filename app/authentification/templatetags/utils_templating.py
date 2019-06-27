9ba9321f63f6e39bd935770c1a1adbf76c1c3db6
Guillim <guillaume.lancrenon@commoprices.com>
2019-06-27 16:18:25 +0200

    small typo about the filter: before it was printing a class "[] input" instead of "input"

--- app/authentification/templatetags/utils_templating.py
+++ app/authentification/templatetags/utils_templating.py
@@ -7,5 +7,5 @@ register = template.Library()
 def addcss(value, arg):
     css_classes = value.field.widget.attrs.get('class', '').split(' ')
     if css_classes and arg not in css_classes:
-        css_classes = '%s %s' % (css_classes, arg)
+        css_classes = '%s %s' % (*css_classes, arg)
     return value.as_widget(attrs={'class': css_classes})
