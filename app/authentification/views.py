736417fb04e99676e5a4af0852f222ffd05f419d
Guillim <guillaume.lancrenon@commoprices.com>
2019-07-01 12:16:40 +0200

    typo fix

--- app/authentification/views.py
+++ app/authentification/views.py
@@ -34,8 +34,7 @@ class SignupView(TemplateView):
             user.is_active = False
             user.save()
             current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
-
+            mail_subject = 'Activate your account.'
             message = render_to_string('acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
