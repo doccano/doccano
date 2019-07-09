b5326fc715f3cbcc5a91f0663db353b3ef5e2981
Guillim <guillaume.lancrenon@commoprices.com>
2019-07-09 23:11:57 +0200

    necessary changes to fake a frontend at first. Changes Will be required on these files

---
+++ app/server/static/components/upload_qanda.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+extends ./upload.pug
+
+block select-format-area
+  label.radio
+    input(
+      type="radio"
+      name="format"
+      value="csv"
+      v-bind:checked="format === 'csv'"
+      v-model="format"
+    )
+    | CSV
+
+  label.radio
+    input(
+      type="radio"
+      name="format"
+      value="json"
+      v-bind:checked="format === 'json'"
+      v-model="format"
+    )
+    | JSONL
+
+block example-format-area
+  pre.code-block(v-show="format === 'plain'")
+    code.plaintext
+      include ./examples/upload_seq2seq.txt
+      | ...
+  pre.code-block(v-show="format === 'csv'")
+    code.csv
+      include ./examples/upload_seq2seq.csv
+      | ...
+  pre.code-block(v-show="format === 'json'")
+    code.json
+      include ./examples/upload_seq2seq.jsonl
+      | ...
+</template>
+
+<script>
+import uploadMixin from './uploadMixin';
+
+export default uploadMixin;
+</script>
