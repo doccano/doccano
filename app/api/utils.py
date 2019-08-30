593b7d85443ecb9043b7c2c79f35f47466b2b91b
Elijah Rippeth <erippeth@mitre.org>
2019-08-30 18:22:35 -0400

    move formerly global into settings.

--- app/api/utils.py
+++ app/api/utils.py
@@ -387,7 +387,7 @@ class SpacyParser(FileParser):
         items = json.loads(file.read())
         data = []
         for i, line in enumerate(items, start=1):
-            if len(data) >= IMPORT_BATCH_SIZE:
+            if len(data) >= settings.IMPORT_BATCH_SIZE:
                 yield data
                 data = []
             try:
