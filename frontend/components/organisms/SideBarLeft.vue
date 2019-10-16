8c386008407ce1eca68ac06568744092640c6eb0
Hironsan <light.tree.1.13@gmail.com>
2019-10-16 22:40:03 +0900

    Remove unused items in sidebar

--- frontend/components/organisms/SideBarLeft.vue
+++ frontend/components/organisms/SideBarLeft.vue
@@ -1,40 +1,8 @@
 <template>
-  <v-list
-    dense
-    class=""
-  >
-    <v-list-item>
-      <v-btn rounded color="white">
-        <v-icon left>
-          mdi-plus
-        </v-icon> Start Labeling
-      </v-btn>
-    </v-list-item>
+  <v-list dense>
     <template v-for="(item, i) in items">
-      <v-layout
-        v-if="item.heading"
-        :key="i"
-        align-center
-      >
-        <v-flex xs6>
-          <v-subheader v-if="item.heading">
-            {{ item.heading }}
-          </v-subheader>
-        </v-flex>
-        <v-flex
-          xs6
-          class="text-right"
-        >
-          <v-btn
-            small
-            text
-          >
-            edit
-          </v-btn>
-        </v-flex>
-      </v-layout>
       <v-divider
-        v-else-if="item.divider"
+        v-if="item.divider"
         :key="i"
         dark
         class="my-4"
@@ -63,22 +31,15 @@
 export default {
   data: () => ({
     items: [
-      // { icon: 'lightbulb_outline', text: 'Start Labeling' },
-      { divider: true },
       { icon: 'mdi-database', text: 'Dataset', link: 'dataset' },
       { icon: 'label', text: 'Labels', link: 'labels' },
       { icon: 'person', text: 'Members', link: 'members' },
       { divider: true },
-      // { heading: 'Labels' },
-      // { icon: 'add', text: 'Create new label', link: 'labels' },
-      // { divider: true },
       { icon: 'backup', text: 'Import', link: 'upload' },
       { icon: 'archive', text: 'Export', link: 'download' },
       { divider: true },
       { icon: 'settings', text: 'Guideline', link: 'guideline' },
-      { icon: 'chat_bubble', text: 'Statistics', link: 'statistics' },
-      { icon: 'help', text: 'Help', link: 'help' }
-      // { icon: 'keyboard', text: 'Keyboard shortcuts' }
+      { icon: 'chat_bubble', text: 'Statistics', link: 'statistics' }
     ]
   })
 }
