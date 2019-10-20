4ce053fa5e1d188bca50668b0531511a9bccc8e9
Hironsan <light.tree.1.13@gmail.com>
2019-10-21 05:53:56 +0900

    Delete organisms/MemberList

--- frontend/components/containers/MemberList.vue
+++ frontend/components/containers/MemberList.vue
@@ -1,22 +1,57 @@
 <template>
-  <member-list
+  <v-data-table
+    :value="selected"
     :headers="headers"
-    :members="items"
-    :selected="selected"
+    :items="items"
+    :search="search"
     :loading="loading"
-    @update-selected="updateSelected"
-    @update-role="updateRole"
-  />
+    loading-text="Loading... Please wait"
+    item-key="id"
+    show-select
+    @input="updateSelected"
+  >
+    <template v-slot:top>
+      <v-text-field
+        v-model="search"
+        prepend-inner-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        filled
+      />
+    </template>
+    <template v-slot:item.role="{ item }">
+      <v-edit-dialog
+        :return-value.sync="item.role"
+        large
+        persistent
+        @save="updateRole({ id: item.id, role: newRole })"
+      >
+        <div>{{ item.role }}</div>
+        <template v-slot:input>
+          <div class="mt-4 title">
+            Update Role
+          </div>
+        </template>
+        <template v-slot:input>
+          <v-select
+            :value="item.role"
+            :items="roles"
+            :rules="roleRules"
+            label="Role"
+            @input="setNewRole"
+          />
+        </template>
+      </v-edit-dialog>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations } from 'vuex'
-import MemberList from '@/components/organisms/MemberList'
+import { roleRules } from '@/rules/index'
 
 export default {
-  components: {
-    MemberList
-  },
   data() {
     return {
       headers: [
@@ -30,7 +65,14 @@ export default {
           text: 'Role',
           value: 'role'
         }
-      ]
+      ],
+      roles: [
+        'Admin',
+        'Member'
+      ],
+      search: '',
+      newRole: null,
+      roleRules
     }
   },
 
@@ -45,6 +87,10 @@ export default {
   methods: {
     ...mapActions('members', ['getMemberList', 'updateMemberRole']),
     ...mapMutations('members', ['updateSelected']),
+
+    setNewRole(value) {
+      this.newRole = value
+    },
 
     updateRole(payload) {
       const data = {
