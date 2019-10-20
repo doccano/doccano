efb3f94021e7399bfa3739b9a878dea15aeed614
Hironsan <light.tree.1.13@gmail.com>
2019-10-20 22:47:30 +0900

    Delete organisms/DocumentList

--- frontend/components/containers/DocumentList.vue
+++ frontend/components/containers/DocumentList.vue
@@ -1,28 +1,74 @@
 <template>
-  <document-list
+  <v-data-table
+    :value="selected"
     :headers="headers"
-    :docs="items"
-    :selected="selected"
+    :items="items"
+    item-key="id"
+    :options.sync="options"
+    :server-items-length="total"
+    :search="search"
     :loading="loading"
-    :total="total"
-    @update-selected="updateSelected"
-    @update-doc="handleUpdateDoc"
-    @change-option="handleChangeOption"
-  />
+    :footer-props="{
+      'items-per-page-options': [10, 50, 100]
+    }"
+    loading-text="Loading... Please wait"
+    show-select
+    @input="updateSelected"
+  >
+    <template v-slot:top>
+      <v-text-field
+        v-model="search"
+        prepend-inner-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        filled
+      />
+    </template>
+    <template v-slot:item.text="{ item }">
+      <v-edit-dialog>
+        <span class="d-flex d-sm-none">{{ item.text | truncate(50) }}</span>
+        <span class="d-none d-sm-flex">{{ item.text | truncate(200) }}</span>
+        <template v-slot:input>
+          <v-textarea
+            :value="item.text"
+            label="Edit"
+            autofocus
+            @change="handleUpdateDocument({ id: item.id, text: $event })"
+          />
+        </template>
+      </v-edit-dialog>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
-import DocumentList from '@/components/organisms/DocumentList'
 
 export default {
-  components: {
-    DocumentList
+  data() {
+    return {
+      search: '',
+      options: {}
+    }
   },
 
   computed: {
     ...mapState('documents', ['items', 'selected', 'loading', 'total']),
     ...mapGetters('documents', ['headers'])
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.getDocumentList({
+          projectId: this.$route.params.id,
+          limit: this.options.itemsPerPage,
+          offset: (this.options.page - 1) * this.options.itemsPerPage
+        })
+      },
+      deep: true
+    }
   },
 
   created() {
@@ -35,20 +81,12 @@ export default {
     ...mapActions('documents', ['getDocumentList', 'updateDocument']),
     ...mapMutations('documents', ['updateSelected']),
 
-    handleUpdateDoc(payload) {
+    handleUpdateDocument(payload) {
       const data = {
         projectId: this.$route.params.id,
         ...payload
       }
       this.updateDocument(data)
-    },
-
-    handleChangeOption(option) {
-      this.getDocumentList({
-        projectId: this.$route.params.id,
-        limit: option.itemsPerPage,
-        offset: (option.page - 1) * option.itemsPerPage
-      })
     }
   }
 }
