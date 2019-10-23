1b6e316a2a773aa6757794d40b302316c237d720
Hironsan <light.tree.1.13@gmail.com>
2019-10-23 14:01:40 +0900

    Enable to non-linear steppers

--- frontend/pages/projects/_id/index.vue
+++ frontend/pages/projects/_id/index.vue
@@ -3,8 +3,8 @@
     <v-card-title>
       Welcome doccano!
     </v-card-title>
-    <v-stepper v-model="e6" vertical>
-      <v-stepper-step :complete="e6 > 1" step="1">
+    <v-stepper v-model="e6" vertical non-linear>
+      <v-stepper-step :complete="e6 > 1" step="1" editable>
         Import a dataset
       </v-stepper-step>
       <v-stepper-content step="1">
@@ -13,42 +13,42 @@
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 2" step="2">Create labels for this project</v-stepper-step>
+      <v-stepper-step :complete="e6 > 2" step="2" editable>Create labels for this project</v-stepper-step>
       <v-stepper-content step="2">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 3" step="3">Add members for collaborative work</v-stepper-step>
+      <v-stepper-step :complete="e6 > 3" step="3" editable>Add members for collaborative work</v-stepper-step>
       <v-stepper-content step="3">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 4" step="4">Define a guideline for the work</v-stepper-step>
+      <v-stepper-step :complete="e6 > 4" step="4" editable>Define a guideline for the work</v-stepper-step>
       <v-stepper-content step="4">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 5">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 5" step="5">Annotate the dataset</v-stepper-step>
+      <v-stepper-step :complete="e6 > 5" step="5" editable>Annotate the dataset</v-stepper-step>
       <v-stepper-content step="5">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 6">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 6" step="6">View statistics</v-stepper-step>
+      <v-stepper-step :complete="e6 > 6" step="6" editable>View statistics</v-stepper-step>
       <v-stepper-content step="6">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 7">Continue</v-btn>
         <v-btn text>Cancel</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 7" step="7">Export the dataset</v-stepper-step>
+      <v-stepper-step :complete="e6 > 7" step="7" editable>Export the dataset</v-stepper-step>
       <v-stepper-content step="7">
         <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
         <v-btn color="primary" @click="e6 = 1">Finish</v-btn>
