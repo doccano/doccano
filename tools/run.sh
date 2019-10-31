e9f22bb1ec3b5cee1e308469c4fc85ba0950be44
Clemens Wolff <clewolff@microsoft.com>
2019-10-31 14:14:58 -0400

    Execute script in app directory

--- tools/run.sh
+++ tools/run.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 set -o errexit
+(
+  cd "$(dirname "$0")/../app"
 
-if [[ ! -d "app/staticfiles" ]]; then python app/manage.py collectstatic --noinput; fi
+  if [[ ! -d staticfiles ]]; then python manage.py collectstatic --noinput; fi
 
-python app/manage.py wait_for_db
-python app/manage.py migrate
-python app/manage.py create_roles
+  python manage.py wait_for_db
+  python manage.py migrate
+  python manage.py create_roles
 
-if [[ -n "${ADMIN_USERNAME}" ]] && [[ -n "${ADMIN_EMAIL}" ]] && [[ -n "${ADMIN_PASSWORD}" ]]; then
-  python app/manage.py create_admin --noinput --username="${ADMIN_USERNAME}" --email="${ADMIN_EMAIL}" --password="${ADMIN_PASSWORD}"
-fi
+  if [[ -n "${ADMIN_USERNAME}" ]] && [[ -n "${ADMIN_EMAIL}" ]] && [[ -n "${ADMIN_PASSWORD}" ]]; then
+    python manage.py create_admin --noinput --username="${ADMIN_USERNAME}" --email="${ADMIN_EMAIL}" --password="${ADMIN_PASSWORD}"
+  fi
 
-gunicorn --bind="0.0.0.0:${PORT:-8000}" --workers="${WORKERS:-1}" --pythonpath=app app.wsgi --timeout 300
+  gunicorn --bind="0.0.0.0:${PORT:-8000}" --workers="${WORKERS:-1}" app.wsgi --timeout=300
+)
