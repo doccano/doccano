bf8938562de63b1873db2c4ba4aad213a8ccca2e
Hironsan <light.tree.1.13@gmail.com>
2019-11-06 11:02:35 +0900

    Update error page

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,14 +1,23 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container
+      fill-height
+      style="height: calc(100vh - 58px);"
+    >
+      <v-layout align-center>
+        <v-flex text-center>
+          <h1 class="display-2 primary--text">Whoops, 404</h1>
+          <p>The page you were looking for does not exist</p>
+          <v-btn
+            to="/"
+            outlined
+            color="primary"
+          >
+            Get me out of here!
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
@@ -31,14 +40,8 @@ export default {
   data() {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
+      otherError: 'The page you were looking for wasn\'t allowed to access.'
     }
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
