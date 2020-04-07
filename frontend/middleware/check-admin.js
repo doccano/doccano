a20a1df9a71eab1e5dceb9be90acb1846dfdb65f
Hironsan <light.tree.1.13@gmail.com>
2020-04-07 14:55:11 +0900

    Remove middleware duplication, resolves #672

--- frontend/middleware/check-admin.js
+++ frontend/middleware/check-admin.js
@@ -1,6 +1,8 @@
 export default function ({ store, route, redirect }) {
   const role = store.getters['projects/getCurrentUserRole']
-  if (!role.is_project_admin) {
-    redirect('/projects/' + route.params.id)
+  const projectRoot = '/projects/' + route.params.id
+  const path = route.fullPath.replace(/\/$/g, '')
+  if (!role.is_project_admin && path !== projectRoot) {
+    return redirect(projectRoot)
   }
 }
