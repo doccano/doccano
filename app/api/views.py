62c1ed56448167da000d826b6f26e8fd286b4800
tusharmakkar08 <tusharmakkar08@gmail.com>
2020-04-08 16:50:18 +0530

    Added user completed data

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,4 @@
+import collections
 import json
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -78,7 +79,8 @@ class StatisticsAPI(APIView):
         if not include or 'label' in include or 'user' in include:
             label_count, user_count = self.label_per_data(p)
             response['label'] = label_count
-            response['user'] = user_count
+            # TODO: Make user_label count chart
+            response['user_label'] = user_count
 
         if not include or 'total' in include or 'remaining' in include:
             progress = self.progress(project=p)
@@ -89,17 +91,27 @@ class StatisticsAPI(APIView):
 
         return Response(response)
 
+    @staticmethod
+    def _get_user_completion_data(annotation_class, annotation_filter):
+        all_annotation_objects  = annotation_class.objects.filter(annotation_filter)
+        set_user_data = collections.defaultdict(set)
+        for ind_obj in all_annotation_objects.values('user__username', 'document__id'):
+            set_user_data[ind_obj['user__username']].add(ind_obj['document__id'])
+        return {i: len(set_user_data[i]) for i in set_user_data}
+
+
     def progress(self, project):
         docs = project.documents
         annotation_class = project.get_annotation_class()
         total = docs.count()
         annotation_filter = Q(document_id__in=docs.all())
+        user_data = self._get_user_completion_data(annotation_class, annotation_filter)
         if not project.collaborative_annotation:
             annotation_filter &= Q(user_id=self.request.user)
         done = annotation_class.objects.filter(annotation_filter)\
             .aggregate(Count('document', distinct=True))['document__count']
         remaining = total - done
-        return {'total': total, 'remaining': remaining}
+        return {'total': total, 'remaining': remaining, 'user': user_data}
 
     def label_per_data(self, project):
         annotation_class = project.get_annotation_class()
