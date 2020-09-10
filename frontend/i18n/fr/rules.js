e42207f8e5201066d917d6d61efff4e7892ddab3
Collin Brown <collinbrown95@gmail.com>
2020-09-10 17:36:19 -0400

    Adding french translations of rules

---
+++ frontend/i18n/fr/rules.js
@@ -0,0 +1,38 @@
+export default {
+  colorRules: {
+    rule1: 'La couleur est obligatoire'
+  },
+  labelNameRules: {
+    rule1: 'Le nom de l\'étiquette est obligatoire',
+    rule2: 'Le nom de l\'étiquette doit comporter moins de 30 caractères'
+  },
+  userNameRules: {
+    rule1: 'Le nom d\'utilisateur est requis',
+    rule2: 'Le nom d\'utilisateur doit comporter moins de 30 caractères'
+  },
+  roleRules: {
+    rule1: 'Rôle est obligatoire'
+  },
+  projectNameRules: {
+    rule1: 'Le nom du projet est requis',
+    rule2: 'Le nom du projet doit comporter moins de 30 caractères'
+  },
+  descriptionRules: {
+    rule1: 'Une description est requise',
+    rule2: 'La description doit comporter moins de 100 caractères'
+  },
+  projectTypeRules: {
+    rule1: 'Le type de projet est requis'
+  },
+  fileFormatRules: {
+    rule1: 'Le format de fichier est requis'
+  },
+  uploadFileRules: {
+    rule1: 'Le fichier est obligatoire',
+    rule2: 'La taille du fichier doit être inférieure à 1MB'
+  },
+  passwordRules: {
+    rule1: 'Le mot de passe est obligatoire',
+    rule2: 'Le mot de passe doit comporter moins de 30 caractères'
+  }
+}
