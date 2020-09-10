0954835afffbad3dc2591870f764358a38cb2111
Collin Brown <collinbrown95@gmail.com>
2020-09-10 17:29:30 -0400

    Adding rules in English

---
+++ frontend/i18n/en/rules.js
@@ -0,0 +1,38 @@
+export default {
+  colorRules: {
+    rule1: 'Color is required'
+  },
+  labelNameRules: {
+    rule1: 'Label name is required',
+    rule2: 'Label name must be less than 30 characters'
+  },
+  userNameRules: {
+    rule1: 'User name is required',
+    rule2: 'User name must be less than 30 characters'
+  },
+  roleRules: {
+    rule1: 'Role is required'
+  },
+  projectNameRules: {
+    rule1: 'Project name is required',
+    rule2: 'Project name must be less than 30 characters'
+  },
+  descriptionRules: {
+    rule1: 'Description is required',
+    rule2: 'Description must be less than 100 characters'
+  },
+  projectTypeRules: {
+    rule1: 'Project type is required'
+  },
+  fileFormatRules: {
+    rule1: 'File format is required'
+  },
+  uploadFileRules: {
+    rule1: 'File is required',
+    rule2: 'File size should be less than 1 MB!'
+  },
+  passwordRules: {
+    rule1: 'Password is required',
+    rule2: 'Password must be less than 30 characters'
+  }
+}
