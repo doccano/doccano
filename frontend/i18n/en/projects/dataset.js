27503b862505b45df78d179ce9e1619a2be0d063
Paul <paul_heinemeyer@outlook.de>
2020-12-14 22:15:59 +0100

    Added dynamic filename and more precise default filename when exporting

--- frontend/i18n/en/projects/dataset.js
+++ frontend/i18n/en/projects/dataset.js
@@ -13,6 +13,7 @@ export default {
   importDataPlaceholder: 'File input',
   exportDataTitle: 'Export Data',
   exportDataMessage: 'Select a file format',
+  exportDataMessage2: 'Select a file name',
   deleteDocumentsTitle: 'Delete Document',
   deleteDocumentsMessage: 'Are you sure you want to delete these documents from this project?',
   pageText: '{0}-{1} of {2}'
