daf290c1e09245efe9fd38e8b37a2bb83447f594
Hironsan <light.tree.1.13@gmail.com>
2021-03-01 08:04:17 +0900

    Add user service to the plugin

--- frontend/plugins/services.ts
+++ frontend/plugins/services.ts
@@ -1,12 +1,18 @@
 import { Plugin } from '@nuxt/types'
 import { FromApiLabelItemListRepository } from '@/repositories/label/api'
 import { FromApiMemberItemListRepository } from '@/repositories/member/api'
+import { FromApiUserItemListRepository } from '@/repositories/user/api'
+import { FromApiRoleItemListRepository } from '@/repositories/role/api'
 import { LabelApplicationService } from '@/services/application/label.service'
 import { MemberApplicationService } from '@/services/application/member.service'
+import { UserApplicationService } from '@/services/application/user.service'
+import { RoleApplicationService } from '@/services/application/role.service'
 
 export interface Services {
   label: LabelApplicationService,
-  member: MemberApplicationService
+  member: MemberApplicationService,
+  user: UserApplicationService,
+  role: RoleApplicationService
 }
 
 declare module 'vue/types/vue' {
@@ -20,9 +26,15 @@ const plugin: Plugin = (context, inject) => {
   const label = new LabelApplicationService(labelRepository)
   const memberRepository = new FromApiMemberItemListRepository()
   const member = new MemberApplicationService(memberRepository)
+  const userRepository = new FromApiUserItemListRepository()
+  const user = new UserApplicationService(userRepository)
+  const roleRepository = new FromApiRoleItemListRepository()
+  const role = new RoleApplicationService(roleRepository)
   const services: Services = {
     label,
-    member
+    member,
+    user,
+    role
   }
   inject('services', services)
 }
