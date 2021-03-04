f4f5217d81debb52bcdcfcbd968720be485eb35c
Hironsan <light.tree.1.13@gmail.com>
2021-03-04 11:25:10 +0900

    Add getMyProfile method to user service

--- frontend/services/application/user.service.ts
+++ frontend/services/application/user.service.ts
@@ -16,6 +16,11 @@ export class UserApplicationService {
     private readonly repository: UserItemListRepository
   ) {}
 
+  public async getMyProfile(): Promise<UserDTO> {
+    const item = await this.repository.getMe()
+    return new UserDTO(item)
+  }
+
   public async list(query: string): Promise<UserDTO[]> {
     const items = await this.repository.list(query)
     return items.map(item => new UserDTO(item))
