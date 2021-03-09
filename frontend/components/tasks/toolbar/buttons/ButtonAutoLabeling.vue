1cef457d3fcc971e29719e85c60c22278bfadad3
Hironsan <light.tree.1.13@gmail.com>
2021-03-09 15:57:51 +0900

    Add auto labeling button

---
+++ frontend/components/tasks/toolbar/buttons/ButtonAutoLabeling.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-btn
+        icon
+        v-on="on"
+        @click="$emit('click:auto')"
+      >
+        <v-icon>
+          mdi-auto-fix
+        </v-icon>
+      </v-btn>
+    </template>
+    <span>Auto Labeling</span>
+  </v-tooltip>
+</template>
