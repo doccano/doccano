aa01a8dd0bb5db18a82cc898b2f1fea1733b0459
Hironsan <light.tree.1.13@gmail.com>
2021-03-11 13:29:32 +0900

    Add text classification repository

---
+++ frontend/repositories/tasks/text-classification/api.ts
@@ -0,0 +1,28 @@
+import ApiService from '@/services/api.service'
+import { TextClassificationItem } from '@/models/tasks/text-classification'
+import { TextClassificationRepository } from './interface'
+
+
+export class FromApiTextClassificationRepository implements TextClassificationRepository {
+  constructor(
+    private readonly request = ApiService
+  ) {}
+
+  public async list(projectId: string, docId: number): Promise<TextClassificationItem[]> {
+    const url = `/projects/${projectId}/docs/${docId}/annotations`
+    const response = await this.request.get(url)
+    const items: TextClassificationItem[] = response.data
+    return items.map(item => TextClassificationItem.valueOf(item))
+  }
+
+  public async create(projectId: string, docId: number, labelId: number): Promise<void> {
+    const url = `/projects/${projectId}/docs/${docId}/annotations`
+    const item = { label: labelId }
+    await this.request.post(url, item)
+  }
+
+  public async delete(projectId: string, docId: number, annotationId: number): Promise<void> {
+    const url = `/projects/${projectId}/docs/${docId}/annotations/${annotationId}`
+    await this.request.delete(url)
+  }
+}
---
+++ frontend/repositories/tasks/text-classification/interface.ts
@@ -0,0 +1,9 @@
+import { TextClassificationItem } from '@/models/tasks/text-classification'
+
+export interface TextClassificationRepository {
+  list(projectId: string, docId: number): Promise<TextClassificationItem[]>
+
+  create(projectId: string, docId: number, labelId: number): Promise<void>
+
+  delete(projectId: string, docId: number, annotationId: number): Promise<void>
+}
