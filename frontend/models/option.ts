f2ea82cfac8044535d8e7ab4b9f08385548ddc40
Hironsan <light.tree.1.13@gmail.com>
2021-03-12 07:31:13 +0900

    Add option model

---
+++ frontend/models/option.ts
@@ -0,0 +1,38 @@
+export class PageNumber {
+  num: number
+
+  constructor(
+    public page: number
+  ) {
+    if (typeof page === 'string' && /^\d+$/.test(page)) {
+      this.num = parseInt(page, 10)
+    }
+    if (typeof page === 'number' && page > 0) {
+      this.num = page
+    }
+    this.num = 1
+  }
+}
+
+export class OptionItem {
+  constructor(
+    public page      : number,
+    public q?        : string,
+    public isChecked?: string
+  ) {}
+
+  static valueOf(
+    { page, q = '', isChecked = '' }:
+    { page: number, q?: string, isChecked?: string }
+  ): OptionItem {
+    return new OptionItem(page, q, isChecked)
+  }
+
+  toObject(): Object {
+    return {
+      page: this.page,
+      q: this.q,
+      isChecked: this.isChecked
+    }
+  }
+}
