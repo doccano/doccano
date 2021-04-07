586748531c5d48ff533a5b8af5c0d7728cadd70d
Hironsan <light.tree.1.13@gmail.com>
2021-04-08 08:34:11 +0900

    Update injest_data task to handle exception

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 
 from .models import Document, Label, Project
 from .serializers import DocumentSerializer, LabelSerializer
+from .views.upload.exception import FileParseException
 from .views.upload.factory import (get_data_class, get_dataset_class,
                                    get_label_class)
 from .views.upload.utils import append_field
@@ -16,36 +17,48 @@ from .views.upload.utils import append_field
 def injest_data(user_id, project_id, filenames, format: str, **kwargs):
     project = get_object_or_404(Project, pk=project_id)
     user = get_object_or_404(get_user_model(), pk=user_id)
-    data_class = get_data_class(project.project_type)
+    response = {'error': []}
+
+    # Prepare dataset.
     dataset_class = get_dataset_class(format)
-    label_class = get_label_class(project.project_type)
     dataset = dataset_class(
         filenames=filenames,
-        label_class=label_class,
-        data_class=data_class,
+        label_class=get_label_class(project.project_type),
+        data_class=get_data_class(project.project_type),
         **kwargs
     )
     annotation_serializer_class = project.get_annotation_serializer()
-    for batch in dataset.batch(settings.IMPORT_BATCH_SIZE):
-        data_serializer = DocumentSerializer(data=batch.data(), many=True)
-        data_serializer.is_valid()
+    it = iter(dataset)
+    while True:
+        try:
+            example = next(it)
+        except StopIteration:
+            break
+        except FileParseException as err:
+            response['error'].append(err.dict())
+            continue
+
+        data_serializer = DocumentSerializer(data=example.data)
+        if not data_serializer.is_valid():
+            continue
         data = data_serializer.save(project=project)
 
         stored_labels = {label.text for label in project.labels.all()}
-        labels = [label for label in batch.label() if label['text'] not in stored_labels]
+        labels = [label for label in example.label if label['text'] not in stored_labels]
         label_serializer = LabelSerializer(data=labels, many=True)
-        label_serializer.is_valid()
+        if not label_serializer.is_valid():
+            continue
         label_serializer.save(project=project)
 
         mapping = {label.text: label.id for label in project.labels.all()}
-        annotation = batch.annotation(mapping)
-        for a, d in zip(annotation, data):
-            append_field(a, document=d.id)
+        annotation = example.annotation(mapping)
+        append_field(annotation, document=data.id)
         annotation_serializer = annotation_serializer_class(
             data=list(itertools.chain(*annotation)),
             many=True
         )
-        annotation_serializer.is_valid()
+        if not annotation_serializer.is_valid():
+            continue
         annotation_serializer.save(user=user)
 
-    return {'error': []}
+    return response
