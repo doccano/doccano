4c192ca8c7cb393711bd79947d8a955f07ad2b80
Johann Frei <johann_frei@yahoo.de>
2021-04-21 21:14:18 +0200

    Refactor paths in scripts

--- offline_deployment/offline_02_2-import_Docker_images.sh
+++ offline_deployment/offline_02_2-import_Docker_images.sh
@@ -5,10 +5,10 @@ cd $DIR
 unset DIR
 
 # Set image tag in Compose to avoid image build
-sed -i 's^dockerfile: app/Dockerfile.prod^dockerfile: app/Dockerfile.prod\n    image: doccano-app:custom^g' ../docker-compose.prod.yml
+sed -i 's^dockerfile: backend/Dockerfile.prod^dockerfile: backend/Dockerfile.prod\n    image: doccano-backend:custom^g' ../docker-compose.prod.yml
 sed -i 's^dockerfile: nginx/Dockerfile^dockerfile: nginx/Dockerfile\n    image: doccano-nginx:custom^g' ../docker-compose.prod.yml
 
 # Load docker images
-docker image load -i doccano-app.tar
+docker image load -i doccano-backend.tar
 docker image load -i doccano-nginx.tar
 docker image load -i postgres.tar
