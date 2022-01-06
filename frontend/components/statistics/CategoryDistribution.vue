d0f0b73fc39b2dfd3214e8fc9e192d6a184e125a
Hironsan <light.tree.1.13@gmail.com>
2022-01-06 16:19:42 +0900

    Add label distribution components

---
+++ frontend/components/statistics/CategoryDistribution.vue
@@ -0,0 +1,27 @@
+<template>
+  <label-distribution
+    title="Category Distribution"
+    :distribution="distribution"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LabelDistribution from './LabelDistribution.vue'
+
+export default Vue.extend({
+  components: {
+    LabelDistribution
+  },
+
+  data() {
+    return {
+      distribution: {}
+    }
+  },
+
+  async created() {
+    this.distribution = await this.$services.statistics.fetchCategoryDistribution(this.$route.params.id)
+  }
+})
+</script>
---
+++ frontend/components/statistics/SpanDistribution.vue
@@ -0,0 +1,27 @@
+<template>
+  <label-distribution
+    title="Span Distribution"
+    :distribution="distribution"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LabelDistribution from './LabelDistribution.vue'
+
+export default Vue.extend({
+  components: {
+    LabelDistribution
+  },
+
+  data() {
+    return {
+      distribution: {}
+    }
+  },
+
+  async created() {
+    this.distribution = await this.$services.statistics.fetchSpanDistribution(this.$route.params.id)
+  }
+})
+</script>
