57d74159ef52bedb1f64ff949f3f6d599a1cd9f1
Hironsan <light.tree.1.13@gmail.com>
2022-03-10 14:13:07 +0900

    Add project creation page

---
+++ frontend/pages/projects/create.vue
@@ -0,0 +1,59 @@
+<template>
+  <form-create
+    v-bind.sync="editedItem"
+    @save="create"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import FormCreate from '~/components/project/FormCreate.vue'
+import { ProjectWriteDTO } from '~/services/application/project/projectData'
+
+export default Vue.extend({
+  components: {
+    FormCreate,
+  },
+
+  layout: 'projects',
+
+  middleware: ['check-auth', 'auth'],
+
+  data() {
+    return {
+      editedItem: {
+        name: '',
+        description: '',
+        projectType: 'DocumentClassification',
+        enableRandomOrder: false,
+        enableShareAnnotation: false,
+        singleClassClassification: false,
+        allowOverlapping: false,
+        graphemeMode: false,
+        useRelation: false,
+      } as ProjectWriteDTO,
+      defaultItem: {
+        name: '',
+        description: '',
+        projectType: 'DocumentClassification',
+        enableRandomOrder: false,
+        enableShareAnnotation: false,
+        singleClassClassification: false,
+        allowOverlapping: false,
+        graphemeMode: false,
+        useRelation: false,
+      } as ProjectWriteDTO,
+    }
+  },
+
+  methods: {
+    async create() {
+      const project = await this.$services.project.create(this.editedItem)
+      this.$router.push(`/projects/${project.id}`)
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+      })
+    },
+  }
+})
+</script>
