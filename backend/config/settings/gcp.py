68ac0157de3ff0166d90e17003a902482261fb95
Hironsan <light.tree.1.13@gmail.com>
2022-04-11 14:48:01 +0900

    Update setting files

--- backend/config/settings/gcp.py
+++ backend/config/settings/gcp.py
@@ -9,6 +9,6 @@ CORS_ORIGIN_WHITELIST = ("http://127.0.0.1:3000", "http://0.0.0.0:3000", "http:/
 CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
 
 DJANGO_DRF_FILEPOND_STORAGES_BACKEND = "storages.backends.gcloud.GoogleCloudStorage"
-GS_BUCKET_NAME = os.environ.get("BUCKET_NAME", "doccano")
-GS_PROJECT_ID = os.environ["GS_PROJECT_ID"]
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+GS_BUCKET_NAME = env("BUCKET_NAME", "doccano")
+GS_PROJECT_ID = env("GS_PROJECT_ID")
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(env("GOOGLE_APPLICATION_CREDENTIALS"))
