35e4827578e90b16521fb7f0daac444b81e8efde
Hironsan <hiroki.nakayama.py@gmail.com>
2022-07-14 15:10:36 +0900

    Update heroku.py

--- backend/config/settings/heroku.py
+++ backend/config/settings/heroku.py
@@ -3,6 +3,7 @@ import django_heroku
 from .base import *  # noqa: F401,F403
 
 django_heroku.settings(locals(), test_runner=False)
+ALLOWED_HOSTS = ['.herokuapp.com']
 LOGGING = {
     'version': 1,
     'handlers': {
