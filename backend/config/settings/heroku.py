5a80b76ed0c787d040bb286d4dd94c99ffbaa582
Hironsan <hiroki.nakayama.py@gmail.com>
2022-07-14 14:51:53 +0900

    Update heroku.py

--- backend/config/settings/heroku.py
+++ backend/config/settings/heroku.py
@@ -3,3 +3,18 @@ import django_heroku
 from .base import *  # noqa: F401,F403
 
 django_heroku.settings(locals(), test_runner=False)
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
+}
