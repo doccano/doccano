3930e89ad48cd1e5e7efd6c6e51729a09a01a865
Hironsan <light.tree.1.13@gmail.com>
2022-10-26 15:12:26 +0900

    Add user creation api

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,8 @@
+from dj_rest_auth.registration.serializers import RegisterSerializer
 from django.contrib.auth.models import User
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters, generics, status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -24,3 +25,19 @@ class Users(generics.ListAPIView):
     pagination_class = None
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ("username",)
+
+
+class UserCreation(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [IsAuthenticated & IsAdminUser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        user = serializer.save(self.request)
+        return user
