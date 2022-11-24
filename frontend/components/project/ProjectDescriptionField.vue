3610d3d0db77ca937f0f65912d5d5997f0c3c30d
Hironsan <hiroki.nakayama.py@gmail.com>
2022-11-24 13:36:11 +0900

    Add project field components

---
+++ frontend/components/project/ProjectDescriptionField.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-text-field
+    v-bind="$attrs"
+    :value="value"
+    :rules="descriptionRules($t('rules.descriptionRules'))"
+    :label="$t('generic.description')"
+    required
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script lang="ts">
+import { descriptionRules } from '@/rules/index'
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data() {
+    return {
+      descriptionRules
+    }
+  }
+})
+</script>
---
+++ frontend/components/project/ProjectNameField.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-text-field
+    v-bind="$attrs"
+    :value="value"
+    :rules="projectNameRules($t('rules.projectNameRules'))"
+    :label="$t('overview.projectName')"
+    required
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script lang="ts">
+import { projectNameRules } from '@/rules/index'
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data() {
+    return {
+      projectNameRules
+    }
+  }
+})
+</script>
