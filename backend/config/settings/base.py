e0f3e3126aa335c8f11d794eb428b335a157cc52
Yoongi Kim <yoongi@yoongi.kim>
2023-01-01 01:02:29 +0900

    기본 SECRET_KEY 변경

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -25,7 +25,7 @@ env.read_env(path.join(BASE_DIR, ".env"), recurse=False)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", "v8sk33sy82!uw3ty=!jjv5vp7=s2phrzw(m(hrn^f7e_#1h2al")
+SECRET_KEY = env("SECRET_KEY", "7ff36e7ae388713db6f9d8e6be67f3f3e9c0438fb7600a23e39ce3ad98c66fbd")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", True)
