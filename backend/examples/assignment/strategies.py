41a254381f06d5db76b557181c62e4be47c7012b
Hironsan <hiroki.nakayama.py@gmail.com>
2023-08-08 20:54:48 +0900

    Simplify weighted sequential strategy

--- backend/examples/assignment/strategies.py
+++ backend/examples/assignment/strategies.py
@@ -45,8 +45,8 @@ class WeightedSequentialStrategy(BaseStrategy):
 
     def assign(self) -> List[Assignment]:
         assignments = []
-        ratio = list(np.round(np.cumsum(self.weights) / 100 * self.dataset_size).astype(int))
-        ratio = [0] + ratio[:-1] + [self.dataset_size]
+        cumsum = np.cumsum([0] + self.weights)
+        ratio = np.round(cumsum / 100 * self.dataset_size).astype(int)
         for user, (start, end) in enumerate(zip(ratio, ratio[1:])):  # Todo: use itertools.pairwise
             assignments.extend([Assignment(user=user, example=example) for example in range(start, end)])
         return assignments
