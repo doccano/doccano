12cb6a9c16f70e46eb4d2a0d64c1eb347613ae1f
GONCALOUNI <a76967@ualg.pt>
2025-02-27 16:47:03 +0000

    removed unusable middleware

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -84,7 +84,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    # "allauth.account.middleware.AccountMiddleware",
 ]
 
 
