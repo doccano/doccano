6dfb56c991d5ae8bbbf774f1602bd9aeb54adb68
GONCALOUNI <a76967@ualg.pt>
2025-03-06 16:42:48 +0000

    Removed middleware

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -84,8 +84,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    # "allauth.account.middleware.AccountMiddleware",
 ]
+
 
 
 ROOT_URLCONF = "config.urls"
