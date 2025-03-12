69694ce0aa4bb65c38ad29b01b11e859ad0aaa04
Catarina Machado <cifmaffm@gmail.com>
2025-03-12 12:23:59 +0000

    chore: update database configuration to use PostgreSQL

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -193,8 +193,12 @@ LOGOUT_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "doccano",
+        "USER": "postgres",
+        "PASSWORD": "doccano",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 # Change 'default' database configuration with $DATABASE_URL.
