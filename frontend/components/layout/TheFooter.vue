ee2a0514eb37bbf96237eb8689aa3e901b6f786f
Steve <a76924@ualg.pt>
2025-03-14 03:51:50 +0000

    New Version

--- frontend/components/layout/TheFooter.vue
+++ frontend/components/layout/TheFooter.vue
@@ -1,34 +1,15 @@
 <template>
-  <v-footer color="black" class="white--text text-center pa-6">
+  <v-footer color="black" class="white--text text-center pa-4">
     <v-container>
-      <v-row justify="center">
-        <!-- Logo -->
-        <v-col cols="12">
-          <v-img src="/logo.png" contain height="50" class="mx-auto"></v-img>
+      <v-row justify="space-between" align="center">
+        <!-- Texto do copyright à esquerda -->
+        <v-col cols="6" class="text-left">
+          &copy; {{ new Date().getFullYear() }} Doccano
         </v-col>
-      </v-row>
 
-      <v-row justify="center" class="mt-3">
-        <v-col cols="12">
-          <p class="mb-0">&copy; {{ new Date().getFullYear() }} Doccano. All rights reserved.</p>
-        </v-col>
-      </v-row>
-
-      <v-row justify="center" class="mt-4">
-        <!-- Ícones de redes sociais -->
-        <v-col cols="12" class="d-flex justify-center">
-          <v-btn icon class="social-icon mx-2" href="https://facebook.com" target="_blank">
-            <v-icon size="24" class="icon-style">mdi-facebook</v-icon>
-          </v-btn>
-          <v-btn icon class="social-icon mx-2" href="https://twitter.com" target="_blank">
-            <v-icon size="24" class="icon-style">mdi-twitter</v-icon>
-          </v-btn>
-          <v-btn icon class="social-icon mx-2" href="https://instagram.com" target="_blank">
-            <v-icon size="24" class="icon-style">mdi-instagram</v-icon>
-          </v-btn>
-          <v-btn icon class="social-icon mx-2" href="https://rss.com" target="_blank">
-            <v-icon size="24" class="icon-style">mdi-rss</v-icon>
-          </v-btn>
+        <!-- Universidade do Algarve à direita -->
+        <v-col cols="6" class="text-right">
+          Universidade do Algarve
         </v-col>
       </v-row>
     </v-container>
@@ -39,32 +20,3 @@
 export default {
 }
 </script>
-
-<style scoped>
-.v-footer {
-  font-size: 14px;
-}
-
-/* Ícones com fundo branco e cor preta */
-.social-icon {
-  background-color: white; /* Fundo branco */
-  color: black !important; /* Cor dos ícones */
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: transform 0.3s ease, background-color 0.3s ease;
-}
-
-.social-icon:hover {
-  transform: scale(1.1);
-  background-color: #ddd;
-}
-
-/* Cor preta nos ícones */
-.icon-style {
-  color: black !important;
-}
-</style>
