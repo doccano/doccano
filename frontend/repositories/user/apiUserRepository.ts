94fcb76c2d4d86274fc780f673f9f1d9359b85c9
Diogo Almeida <75150429+diogolem@users.noreply.github.com>
2025-03-26 11:26:07 +0000

    added update

--- frontend/repositories/user/apiUserRepository.ts
+++ frontend/repositories/user/apiUserRepository.ts
@@ -2,6 +2,7 @@ import { Page } from '@/domain/models/page'
 import { UserItem } from '@/domain/models/user/user'
 import ApiService from '@/services/api.service'
 
+
 function toModel(item: { [key: string]: any }): UserItem {
   return new UserItem(
     item.id,
@@ -15,16 +16,14 @@ function toModel(item: { [key: string]: any }): UserItem {
   )
 }
 
-
-
 function toPayload(item: { [key: string]: any }): { [key: string]: any } {
   return {
     username: item.username,
+    first_name: item.first_name,
+    last_name: item.last_name,
     email: item.email,
     password1: item.password1,
     password2: item.password2,
-    first_name: item.first_name,
-    last_name: item.last_name,
     is_superuser: item.is_superuser,
     is_staff: item.is_staff
   }
@@ -83,4 +82,15 @@ export class APIUserRepository {
     console.log('Resposta do backend:', response.data);
     return toModel(response.data)
   }
+
+  async update(userId: number, fields: { [key: string]: any }): Promise<UserItem> {
+  const url = `/users/${userId}/update`; // Define the update endpoint
+  const payload = toPayload(fields);
+  console.log('Updating user with payload:', payload);
+
+  const response = await this.request.put(url, payload);
+  return toModel(response.data);
+
+}
+
 }
