261eb6adb289d572e00783f019c2b952f558a2db
Diogo Almeida <75150429+diogolem@users.noreply.github.com>
2025-03-26 11:33:57 +0000

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 from .views import Me, UserCreation, Users
 from .views import BulkDeleteUsers
+from .views import UserUpdateView
 
 urlpatterns = [
     path(route="me", view=Me.as_view(), name="me"),
@@ -10,4 +11,5 @@ urlpatterns = [
     path("auth/", include("dj_rest_auth.urls")),
     path('users', BulkDeleteUsers.as_view(), name='user_bulk_delete'),
     path("users/delete", BulkDeleteUsers.as_view(), name="user_bulk_delete"),
+    path('users/<int:pk>/update', UserUpdateView.as_view(), name='user-update'),
 ]
